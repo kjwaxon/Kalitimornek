9ef469687956633ba08ac490aac33cc78a1c8de3
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:44:52 +0300

    Kalıtım örnek

---
+++ ConsoleAppKalıtımörnek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKalıtımörnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samsung s = new Samsung();
+            s.isim = "S21";
+            s.isletimsistemi = "Android 12";
+            s.hafızaalanı = 256;
+            s.telefoninfo();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppKalıtımörnek/Telefon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKalıtımörnek
+{
+    public class Telefon
+    {
+        public string isim;
+        public string isletimsistemi;
+        public int hafızaalanı;
+
+        public void telefoninfo()
+        {
+            Console.WriteLine("Telefonun ismi : "+isim);
+            Console.WriteLine("Telefonun işletim sistemi : " + isletimsistemi);
+            Console.WriteLine("Telefonun hafıza alanı : {0} gb",hafızaalanı);
+        }
+    }
+}
